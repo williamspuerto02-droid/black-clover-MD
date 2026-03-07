4452b2ba4c401e40210293762b47625e47a47559
Sifu <williamspuerto02@gmail.com>
2026-03-07 17:04:39 -0600

    Update main-menu2.js

--- plugins/main-menu2.js
+++ plugins/main-menu2.js
@@ -1,5 +1,5 @@
 let handler = async (m, { isPrems, conn }) => {
-  let img = 'https://files.catbox.moe/6u1gfe.jpg';
+  let img = 'https://files.catbox.moe/fdv4u9.jpg';
   let texto = `*☁️ _M E N U - A U D I O S_ ☁️*
 
 ° _Tunometecabrasaramambiche_.
