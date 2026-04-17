e4f21f6812127e5004e9a30bb741ecd8b30ed956
Sifu <williamspuerto02@gmail.com>
2026-04-17 16:38:01 -0500

    Update start.js

--- núcleo•clover/start.js
+++ núcleo•clover/start.js
@@ -135,7 +135,7 @@ const connectionOptions = {
     logger: pino({ level: 'silent' }),
     printQRInTerminal: opcion == '1' ? true : methodCodeQR ? true : false,
     mobile: MethodMobile, 
-    browser: opcion == '1' ? [`${global.nameqr}`, 'Chrome', '20.0.04'] : methodCodeQR ? [`${global.nameqr}`, 'Chrome', '20.0.04'] : ['Ubuntu', 'Chrome', '110.0.1587.56'],
+    browser: opcion == '1' ? [`${global.nameqr}`, 'Edge', '20.0.04'] : methodCodeQR ? [`${global.nameqr}`, 'Edge', '20.0.04'] : ['Ubuntu', 'Edge', '110.0.1587.56'],
     auth: {
         creds: state.creds,
         keys: makeCacheableSignalKeyStore(state.keys, Pino({ level: "fatal" }).child({ level: "fatal" })),
