274352e7a527c23abd6588a959d1bc64d0e81e91
Sifu <williamspuerto02@gmail.com>
2026-05-30 14:30:26 -0500

    Update handler.js

--- núcleo•clover/handler.js
+++ núcleo•clover/handler.js
@@ -269,7 +269,7 @@ export async function handler(chatUpdate) {
         premium: `*REQUIERE CUENTA PREMIUM*\n\n> 🚫 Módulo exclusivo para usuarios *𝙑𝙄𝙋 - 𝙋𝙍𝙀𝙈𝙄𝙐𝙈*.\n\n📡 Actualiza tu plan con: */vip*\n⚙️ Estado: denegado`,
         private: `🔒 *SOLO CHAT PRIVADO* 📲\n\n> Este comando no puede ejecutarse en grupos por razones de seguridad.\n\n🧬 Ejecuta este protocolo directamente en el chat privado.`,
         admin: `🛡️ *FUNCIÓN RESTRINGIDA*\n\n> Solo los administradores del *Grupo* tienen acceso.\n\n⚠️ Intento no autorizado.`,
-        unreg: `📄 *NO REGISTRADO EN EL SISTEMA*\n\n> 🚫 *Acceso denegado:* No puedes usar los comandos sin registrarte.\n\n🔐 Regístrate con: */reg nombre.edad*\n📍 Ejemplo: */reg Sifu.20*\n\n> *Creditos a:* ᵗʰᵉ ᶜᵃʳˡᵒˢ`,
+        unreg: `📄 *NO REGISTRADO EN EL SISTEMA*\n\n> 🚫 *Acceso denegado:* No puedes usar los comandos sin registrarte.\n\n🔐 Regístrate con: */reg nombre.edad*\n📍 Ejemplo: */reg Sifu.20*\n\n> *https://files.catbox.moe/dapzy2.jpg\n\nCreditos a:* ᵗʰᵉ ᶜᵃʳˡᵒˢ`,
         restrict: `🚷 *FUNCIÓN GLOBALMENTE BLOQUEADA*\n\n> Este comando fue deshabilitado por el *Operador Global* por motivos de seguridad cibernética.\n\n🔧 Módulo: /xvideos`
         }[type];
 if (msg) return m.reply(msg).then(_ => m.react('✖️'))}
